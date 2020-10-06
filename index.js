d960e39bf97a2cd9e281f99ef80df5ec84141b06
nikichumpitaz <71062106+nikichumpitaz@users.noreply.github.com>
2020-10-05 22:37:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+
+class MysteriousMan {
+
+    constructor(friends){
+      this.name = 'Mysterious Man';
+      this.home = 'sketchy alley';
+      this.friends = [none];
+      this.themeSong = somesadboimusic; 
+      this.seeking = ['friends', 'self-esteem', 'joy in life'];
+      this.route = 'park';
+  
+    }
+  
+    if (friends > 0) {
+     result = happiness;
+    } else {
+    result = cry;   
+    }
+    return result;
+    }
+
+
+    befriend(){
+      //he's shy and really bad at making friends
+      if (spoken to) {
+        say (stay with me forever)
+      }
+    }
+
+    seekfriends(){
+      //wants friends, but his knife collection scares people off
+      if (approached) {
+       result = show knife-collection;
+      } else {
+      action = approach;
+       result = show knife-collection;
+      }
+      return result;
+      }
+    }
+    
+    cry(){
+      //crys when rejected
+       }
+  
+  }
